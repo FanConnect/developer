eec7be8b5010836ab416ca350f7ad0a8b8e13740
Keith Layne <2667202+keithlayne@users.noreply.github.com>
2023-08-04 15:00:50 -0400

    Update preact example with template-sdk

--- examples/preact/boilerplate/src/index.tsx
+++ examples/preact/boilerplate/src/index.tsx
@@ -1,25 +1,33 @@
+import { scale } from '@fanconnecttv/template-sdk';
 import { render } from 'preact';
+import { useEffect } from 'preact/hooks';
 
-/*import { getContentOptions } from '~lib';
+/*
+import { getContentOptions } from '~lib';
 import { type Config } from './config.json';
-import { scale } from '';*/
+*/
 
 interface BoilerplateProps {
   message: string;
 }
 
 const Boilerplate = ({ message }: BoilerplateProps) => {
+  useEffect(FC.initialized, []);
   return <div>{message}</div>;
 };
 
-FC.addEventListener('initialize', async ({ contentOptions, device }) => {
+FC.addEventListener('initialize', async ({ /* contentOptions,*/ options }) => {
   scale(document.body, 1920, 1080);
 
-  const options = getContentOptions<Config>(contentOptions);
+  // TODO
+  // const options = getContentOptions<Config>(contentOptions);
+
+  // const { message } = options;
 
   const { message } = options;
 
-  if (message == null) return FC.skip(`null message`);
+  if (typeof message !== 'string') return FC.skip(`null message`);
+  // if (message == null) return FC.skip(`null message`);
 
   render(<Boilerplate message={message} />, document.body);
 });
